3400c57822114ba412eeca2d809347b92a1f0546
fahdiazhannu31 <fahdiazhannu31@gmail.com>
2024-09-17 16:41:11 +0700

    uploaaaaad

--- SimpleTest.php
+++ SimpleTest.php
@@ -16,7 +16,7 @@ class SimpleTest extends TestCase
         $Wc = new WordCount();
 
         // Kita masukan parameter 4 kata, yang harusnya dapat output 4.
-        $TestSentence = "My name is Azhannu"; // 4 Kata ..
+        $TestSentence = "My name is Janu"; // 4 Kata ..
         $WordCount = $Wc->countWords($TestSentence);
 
         // Kita assert equal, ekspektasi nya harus 4, jika benar berarti Wordcount berfungsi dengan baik.
